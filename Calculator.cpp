d2901556458ddba0bdf43a8f3bd4513098b8e895
AEksel <akselgab@gmail.com>
2024-02-06 11:18:47 +0100

    Finish Simple Calculator very basic, might make a gui one later
    #1

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,6 +5,35 @@ using namespace std;
 
 int main()
 {
+    // Variables
+    string operation;
+    double number1, number2;
+
+    // User Input
+    cout << "Enter inital number: ";
+    cin >> number1;
+        cout << "Which operation= (+ - * /): ";
+    cin >> operation;
+        cout << "Enter second number: ";
+    cin >> number2;
+
+
+    // check operation
+    if (operation == "+")
+    {
+        cout << number1 + number2;
+    } else if (operation == "-") {
+        cout << number1 - number2;
+    } else if (operation == "*") {
+        cout << number1 * number2;
+    } else if (operation == "/") {
+        cout << number1 / number2;
+    } else 
+    {
+        cout << "Invalid operation";
+    }
+    
+
 
     return 0;
 }
